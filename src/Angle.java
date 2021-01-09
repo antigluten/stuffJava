02abcc435766da8eeb6794effb20a46156dfa553
Django-explorer <bariy.litievich@gmail.com>
2021-01-09 17:32:50 +0300

    Реализовал метод смещения точки по углу

--- src/Angle.java
+++ src/Angle.java
@@ -1,10 +1,10 @@
 public class Angle {
-    static double findAngle (double xOne, double yOne, double xTwo, double yTwo){
+    static double findAngle (double xOne, double yOne, double xTwo, double yTwo) {
         return Math.acos((yOne*yTwo+xOne*xTwo)/Math.sqrt(Math.pow(yOne,2)+Math.pow(xOne,2))*Math.sqrt(Math.pow(yTwo,2)+Math.pow(xTwo,2)));
     }
 
 }
-class AngleTest{
+class AngleTest {
     public static void main(String[] args) {
         System.out.println(Math.toDegrees(Angle.findAngle(1.0, 0.0, 0.0, 1.0)));
     }
